6a30b147dbcb900f7cd09d220b58de74f0bbda2e
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-05-01 20:17:14 -0400

    Starting off with navbar and header functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,45 @@
-import React from "react";
-import Logo from 'src/assets/logo.png'
-import { FaBars } from "react-icons/fa";
+import React, {useState} from "react";
+import Logo from '../assets/logo.png'
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className=" fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo" style={{ width: "50px" }} />
       </div>
 
-      <div>
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+
+        <ul className={ !nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">About</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Work</li>
+          <li className="py-6 text-4xl">Contact</li>
+        </ul>
+
+
+      <div className="flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
+            <li></li>
         </ul>
-      </div>
 
-      <div>
-        <FaBars />
-      </div>
 
-      <div>
-        <ul className="hidden">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
       </div>
-
-      <div></div>
     </div>
   );
 }
